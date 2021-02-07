b070159f56c9195773188a3692e202729ae8c48b
Jonas Almeida <jalmeida@mathbiol.org>
2021-02-07 02:01:32 -0500

    get individual sequence

--- ucovid.js
+++ ucovid.js
@@ -9,7 +9,7 @@ ucovid.genomes=async (url)=>{ // list with results will be stored at ucovid.geno
     // default url, note cache in localforage
     url=url||"https://api.ncbi.nlm.nih.gov/datasets/v1alpha/virus/taxon/2697049/genome/table?refseq_only=false&annotated_only=true&host=9606&complete_only=true&table_fields=nucleotide_accession&table_fields=species_tax_id&table_fields=species_name&table_fields=genus&table_fields=family&table_fields=isolate_name&table_fields=nucleotide_length&table_fields=sequence_type&table_fields=nuc_completeness&table_fields=geo_location&table_fields=us_state&table_fields=host_name&table_fields=host_tax_id&table_fields=collection_date&table_fields=bioproject&table_fields=biosample&format=jsonl";
     if(!ucovid.genomes.list){
-        if(typeof(localforage!='undefined')){ // if localforage exists
+        if(typeof(localforage)!='undefined'){ // if localforage exists
             ucovid.genomes.list= await localforage.getItem('ucovid.genomes.list')
         }
         if(!ucovid.genomes.list){
@@ -21,6 +21,13 @@ ucovid.genomes=async (url)=>{ // list with results will be stored at ucovid.geno
         }   
     }
     return ucovid.genomes.list
+}
+
+ucovid.getSequence= async (acc='LC528232.1')=>{ // get fasta for genome by accession id
+    //let txt = await(await fetch(`https://www.ncbi.nlm.nih.gov/search/api/download-sequence/?db=nuccore&id=${acc}`)).txt()
+    // https://www.ncbi.nlm.nih.gov/sviewer/viewer.fcgi?id=1968818726&report=fasta
+    let txt = await (await fetch(`https://www.ncbi.nlm.nih.gov/sviewer/viewer.fcgi?id=${acc}&db=nuccore&report=fasta`)).text()
+    return txt
 }
 
 ucovid.getLocalForage=()=>{
