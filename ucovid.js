058f4f4479908fff5540049047813e93a72661cf
Jonas Almeida <jalmeida@mathbiol.org>
2021-02-09 06:42:20 -0500

    update genome list

--- ucovid.js
+++ ucovid.js
@@ -1,13 +1,13 @@
 console.log('ucovid.js loaded')
 
 ucovid={
-    date:new Date()
+    date:new Date(),
+    genomeListURL:"https://api.ncbi.nlm.nih.gov/datasets/v1alpha/virus/taxon/2697049/genome/table?refseq_only=false&annotated_only=true&host=9606&complete_only=true&table_fields=nucleotide_accession&table_fields=species_tax_id&table_fields=species_name&table_fields=genus&table_fields=family&table_fields=isolate_name&table_fields=nucleotide_length&table_fields=sequence_type&table_fields=nuc_completeness&table_fields=geo_location&table_fields=us_state&table_fields=host_name&table_fields=host_tax_id&table_fields=collection_date&table_fields=bioproject&table_fields=biosample&format=jsonl"
 }
 
 
-ucovid.genomes=async (url)=>{ // list with results will be stored at ucovid.genomes.list
+ucovid.genomes=async (url=ucovid.genomeListURL)=>{ // list with results will be stored at ucovid.genomes.list
     // default url, note cache in localforage
-    url=url||"https://api.ncbi.nlm.nih.gov/datasets/v1alpha/virus/taxon/2697049/genome/table?refseq_only=false&annotated_only=true&host=9606&complete_only=true&table_fields=nucleotide_accession&table_fields=species_tax_id&table_fields=species_name&table_fields=genus&table_fields=family&table_fields=isolate_name&table_fields=nucleotide_length&table_fields=sequence_type&table_fields=nuc_completeness&table_fields=geo_location&table_fields=us_state&table_fields=host_name&table_fields=host_tax_id&table_fields=collection_date&table_fields=bioproject&table_fields=biosample&format=jsonl";
     if(!ucovid.genomes.list){
         if(typeof(localforage)!='undefined'){ // if localforage exists
             ucovid.genomes.list= await localforage.getItem('ucovid.genomes.list')
@@ -21,6 +21,17 @@ ucovid.genomes=async (url)=>{ // list with results will be stored at ucovid.geno
         }   
     }
     return ucovid.genomes.list
+}
+
+ucovid.updateGenomes=async(url=ucovid.genomeListURL)=>{
+    if(typeof(localforage)=='undefined'){
+        console.log('localforage persistency not available in this machine')
+    }else{
+        let txt = await(await fetch(url)).text()
+        ucovid.genomes.list = JSON.parse(`[${txt.replace(/[\n\r]+/g,',').slice(0,-1)}]`)
+        localforage.setItem('ucovid.genomes.list',ucovid.genomes.list)
+        return ucovid.genomes.list
+    }   
 }
 
 ucovid.getSequence= async (acc='LC528232.1')=>{ // get fasta for genome by accession id
